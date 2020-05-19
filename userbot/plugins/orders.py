c0184f3064b88bc99ea0172780cbad50cea54415
SourcePrivet <62123708+SourcePrivet@users.noreply.github.com>
2020-05-19 20:58:52 +0300

    Create orders.py

---
+++ userbot/plugins/orders.py
@@ -0,0 +1,74 @@
+"""Emoji
+Available Commands:
+.dh"""
+
+from telethon import events
+
+import asyncio
+
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd("(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(0, 5)
+    input_str = event.pattern_match.group(1)
+    if input_str == "dh":
+        await event.edit(input_str)
+        animation_chars = [
+            ".moon
+.smoon
+.earth
+.fap
+.Oof
+.heart
+.lovestory
+.fuk
+.wtf
+.ok
+.hack
+.pornhub
+.lucky
+.heart
+.load
+.square
+.up
+.round
+.anim
+.monkey
+.fnl
+.hand
+.cnt
+.kk
+.clock
+.congo
+.ding
+.dfork
+.eye
+.chod
+.sqh
+.vquickheal
+.fuck
+.sux
+.kiss
+.windows
+.macos
+.linux
+.stock
+.os
+.snake
+.squ
+.think
+.lol
+.virus
+.wahack
+by @visa4bin",
+           
+        ]
+
+        for i in animation_ttl:
+        	
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i %5 ])
