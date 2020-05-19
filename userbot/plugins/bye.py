bd1b327857dd708ce2b310becf0def63b51eda06
SourcePrivet <62123708+SourcePrivet@users.noreply.github.com>
2020-05-19 20:28:14 +0300

    Update bye.py

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -26,7 +26,7 @@ async def leave(e):
 
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
 
-        await e.edit("`Jaa Rha Hu Mai Toh Bc Gaand Marao.....!`")
+        await e.edit("`مغادر، لتحل عليكم لعنتي `")
 
         time.sleep(3)
 
